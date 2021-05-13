02354d6a2c2a5cebe71cabc94a4ef5b3a8977508
Mike Jones <mike@intelligentediting.com>
2021-05-13 09:57:47 +0100

    Add inital chapter 8

---
+++ FunctionalCSharp/Chapter8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCSharp.Chapter8
+{
+    public static class Exercise
+    {
+        // 1. Implement Apply for Either and Exceptional.
+
+        // 2. Implement the query pattern for Either and Exceptional. Try to write down the signatures for Select and SelectMany without looking at the examples
+        //    for the implementation, just follow the types-fi it type checks, it's probably right!
+
+        // 3. Come up with a scenario in which various Either-returning operations are chained with Bind. *If you're short of ideas, you can use the favorite-dish
+        //    example for chapter 6.) Rewrite the code using a LINQ expression.
+    }
+}
